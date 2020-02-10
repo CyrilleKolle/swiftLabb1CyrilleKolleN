af35767e514131f3791f949befad1e69320502f5
CyrilleKolle <ngidekollecyrille@gmail.com>
2020-02-10 13:38:20 +0100

    Initial Commit

---
+++ swiftLabb1CyrilleKolleN/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swiftLabb1CyrilleKolleN
+//
+//  Created by Indigo´sDad on 2020-02-10.
+//  Copyright © 2020 Indigo´sDad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
